f7c08db66431a00799bf989906163665db2b3dfc
baseerkhan034 <baseerkhan034@gmail.com>
2025-03-19 18:53:17 -0400

    Samplereader and sample file added

---
+++ Sample.java
@@ -0,0 +1,32 @@
+package collections;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int c=4;
+		int d=3;
+		int e=2;
+		
+		int x= (a+b-c*d/e);
+		
+		System.out.println(x);
+		
+		
+		/*
+		List<String> groc = new ArrayList<String>();
+		groc.add("apple");
+		groc.add("\nbanana");
+		groc.add("\nchia seeds");
+		groc.add("\nmustard");
+		groc.add("\nkale");
+		groc.add("\nspinach");
+		groc.add("\nlady fingers");
+		
+		System.out.println(groc);
+		//System.out.println(groc.get(4));
+		*/
+	}
+
+}
